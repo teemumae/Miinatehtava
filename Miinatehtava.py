a884fe6497f852794a83433277b134d11ecd062a
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-03 00:14:10 +0300

    Pääohjjelman muokkauksia

--- Miinatehtava.py
+++ Miinatehtava.py
@@ -7,4 +7,7 @@ TULOSTUKSET = {
 def sijainti_kentalla(x, y, leveys, korkeus):
     return sijainti
 def tulosta_sijainti(sijainti):
+    print(TULOSTUKSET[sijainti])
 
+leveys = int(input("Anna kentän leveys: "))
+korkeus = int(input("Anna kentän korkeus: "))
