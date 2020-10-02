22445baddc4449a9c587411c16954a34afa8c77a
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-03 00:23:12 +0300

    Pääohjelma valmis

--- Miinatehtava.py
+++ Miinatehtava.py
@@ -6,8 +6,17 @@ TULOSTUKSET = {
 }
 def sijainti_kentalla(x, y, leveys, korkeus):
     return sijainti
+
 def tulosta_sijainti(sijainti):
     print(TULOSTUKSET[sijainti])
 
 leveys = int(input("Anna kentän leveys: "))
 korkeus = int(input("Anna kentän korkeus: "))
+
+if leveys <= 0 or korkeus <= 0:
+    print("Noin pienelle kentälle ei mahdu ainuttakaan ruutua!")
+else:
+    x = int(input("Anna x-koordinaatti:  "))
+    y = int(input("Anna y-koordinaatti:  "))
+    sijainti = sijainti_kentalla(x, y, leveys, korkeus)
+    tulosta_sijainti(sijainti)
