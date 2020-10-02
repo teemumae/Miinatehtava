9d512eb2a18d9a574b6bb195bcb82b484f6983ce
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-02 22:39:15 +0300

    Add project files.

---
+++ Miinatehtava.py
@@ -0,0 +1,9 @@
+TULOSTUKSET = {
+    "ulkona": "Antamasi ruutu on kentän ulkopuolella.",
+    "nurkassa": "Antamasi ruutu on kentän nurkassa.",
+    "laidalla": "Antamasi ruutu on kentän laidalla.",
+    "keskellä": "Antamasi ruutu on keskikentällä."
+}
+def sijainti_kentalla(x, y, leveys, korkeus):
+
+def tulosta_sijainti(sijainti):
